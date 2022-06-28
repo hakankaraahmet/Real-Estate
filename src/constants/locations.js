0b72989ea22c892e71778ba2a2fd5b2a115d3fe2
Hakan Karaahmetoglu <81388445+hakankaraahmet@users.noreply.github.com>
2022-06-28 15:41:56 +0300

    locations images are added and locations data is prepared

---
+++ src/constants/locations.js
@@ -0,0 +1,25 @@
+import California from "../assets/california.jpg";
+import NewYork from "../assets/newyork.jpg";
+import Ny from "../assets/ny.jpg";
+
+export const locations = [
+  {
+    id: 0,
+    img: California,
+    title: "New York",
+    content:
+    "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that's among the worl's major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.",
+  },
+  {
+    id: 1,
+    img: NewYork,
+    title: "San Francisco",
+    content:  "California is a state in the Western United States. California borders Oregon to the north, Nevada and Arizona to the east, the Mexican state of Baja California to the south; and has a coastline along the Pacific Ocean to the west.",
+  },
+  {
+    id: 2,
+    img: Ny,
+    title: "New York",
+    content:   "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that's among the worl's major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.",
+  },
+];
